5044034feeaabead928d129780476992043d2c2d
Kevin Wilson <kevin@tripsis.co.uk>
2013-03-21 12:39:40 +0000

    Added StyleCop rules and home page.

--- Ibi.Fe.ApiTests/Controllers/HomeController.cs
+++ Ibi.Fe.ApiTests/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="IBI Group">
+//   (c) Copyright IBI Group. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// </copyright>
+// <summary>
+//   Defines the HomeController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Ibi.Fe.ApiTests.Controllers
 {
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Main controller for home page items.
+    /// </summary>
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
+        /// <summary>
+        /// Main home page.
+        /// </summary>
+        /// <returns>A view with the main home page.</returns>
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
